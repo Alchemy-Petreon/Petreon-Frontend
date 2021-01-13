8ef623a721d8a1eef5235d1e0d0d617f31626877
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-12 20:42:11 -0800

    landing page banner created

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,10 +5,10 @@ export default class LandingPage extends Component {
         return (
             <div>
                 <div className="banner">
-                    <img src="http://placekitten.com/1000/250" alt="kitties" />
+                    <img src="/five-little-grey-kittens.jpg" alt="kitties" className="landing-banner"/>
+                    <h1 className="monetize">Monetize Your Monster</h1>
                 </div>
                 <div className="intro">
-                    <h1>Monetize Your Monster</h1>
                     <p>Petreon is an application where pet owners can showcase their pets by posting images, videos, and/or blogs while soliciting donations from followers. Users can choose pets to follow and curate their dash as well as like and comment on posts they they enjoy.</p>
                 </div>
             </div>
