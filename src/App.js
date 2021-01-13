f3e48b37e702fb6317a8fd2425e9c75bd948cd5a
Sjaan H <sjaanahydrick@gmail.com>
2021-01-12 19:20:09 -0800

    Feature/front end app.js routing (#1)
    * added App.js with LandingPage route
    
    * added LandingPage.js; confirmed routing to App.js was correct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './style/App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import LandingPage from './LandingPage.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route
+            path = "/"
+            exact render={(routerProps) => <LandingPage {...routerProps} />}
+            />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
