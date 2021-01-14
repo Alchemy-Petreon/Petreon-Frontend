832e5bef200c1d3a0076068a149b56e872fef641
Evan Finkelstein <evanfinkelstein91@gmail.com>
2021-01-14 14:59:15 -0800

    create pet page with form

---
+++ src/Create-pet.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { createPet } from "./fetches/pet-fetches.js"
+
+
+export default class CreatePet extends Component {
+    state = {
+        petName: '',
+        type: '',
+        petProfilePicture: '',
+        petProfileDescription: '',
+        bannerPicture: ''
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(this.state);
+
+        this.setState({ loading: true })
+        const pet = {
+            userId: this.props.userId,
+            petName: this.state.petName,
+            type: this.state.type,
+            petProfilePicture: this.state.petProfilePicture,
+            petProfileDescription: this.state.petProfileDescription,
+            bannerPicture: this.state.bannerPicture
+        }
+        createPet(pet);
+
+        this.props.history.push('/');
+
+    }
+    render() {
+        return (
+            <div className='create-pet-page'>
+                <div className='box'>
+                    <h2 className='create-pet'> Sign Up</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <p className='pet-name'>Pet Name:</p>
+                        <input onChange={(e) => this.setState({ petName: e.target.value })}
+                            value={this.state.petName}></input>
+                        <p className='pet-type'>Pet Type:</p>
+                        <input onChange={(e) => this.setState({ type: e.target.value })}
+                            value={this.state.type} ></input>
+                        <p className='pet-picture'>Pet Profile Picture URL:</p>
+                        <input onChange={(e) => this.setState({ petProfilePicture: e.target.value })}
+                            value={this.state.petProfilePicture} ></input>
+                        <p className='pet-description'>Pet Description:</p>
+                        <input onChange={(e) => this.setState({ petProfileDescription: e.target.value })}
+                            value={this.state.petProfileDescription} ></input>
+                        <p className='pet-banner'>Banner Picture URL:</p>
+                        <input onChange={(e) => this.setState({ bannerPicture: e.target.value })}
+                            value={this.state.bannerPicture} ></input>
+                        <br />
+                        <button className='create-pet-button'>Submit</button>
+                        <br />
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './style/Footer.css';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer">
+                <span className="foot-logo">Petreon</span>
+                <Link to="/aboutus"><span className="about-us">About Us</span></Link>
             </div>
         )
     }
