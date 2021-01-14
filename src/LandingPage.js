b086b0eedd70a7833a64c85261a35b2736ddac07
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-13 20:54:06 -0800

    landing page fully styled #19

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
+            <div className="lp">
                 <div className="banner">
                     <img src="/five-little-grey-kittens.jpg" alt="kitties" className="landing-banner"/>
                 </div>
@@ -15,8 +15,34 @@ export default class LandingPage extends Component {
                     <div className="naplesyellow"> </div>
                 </div>
                 <div className="intro">
-                    <h2>Showcase your pet!</h2> 
-                    <p>by posting images, videos, and/or blogs while soliciting donations from followers. Users can choose pets to follow and curate their dash as well as like and comment on posts they they enjoy.</p>
+                    <h2 className="showcase">Promote your pet!</h2>
+                    <p className="lp1">Share your best friend with the world by posting images, videos, or blogs! Like and comment on posts to build a community with fans!</p>
+                    <img src="/ben1.jpg" alt="ben" className="ben" />
+
+                    <div className="divider1">
+                        <div className="midnightgreendiv1"> </div>
+                        <div className="bittersweetdiv1"> </div>
+                        <div className="naplesyellowdiv1"> </div>
+                    </div>
+
+                    <h2 className="follow">Follow your favorites!</h2>
+                    <p className="lp2">Curate your dashboard to only include content from the pets you want to see the most!</p>
+                    <img src="/Vinewood.png" alt="vinewood" className="vinewood" />
+
+                    <div className="divider2">
+                        <div className="midnightgreendiv2"> </div>
+                        <div className="bittersweetdiv2"> </div>
+                        <div className="naplesyellowdiv2"> </div>
+                    </div>
+
+                    <h2 className="monetize">Monetize your monster!</h2>
+                    <p className="lp3">Accept tips from people who want the best for your beast!</p>
+                    <img src="/Tulsi.jpg" alt="tulsi" className="tulsi" />
+                </div>
+                <div className="footer">
+                    <div className="midnightgreenfoot"> </div>
+                    <div className="bittersweetfoot"> </div>
+                    <div className="naplesyellowfoot"> </div>
                 </div>
             </div>
         )
