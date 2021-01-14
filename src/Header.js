ec9f4f632e5f9120071116f489fdd8938df443ac
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-14 13:07:05 -0800

    Header in progress; pushing for pulling

--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,17 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <Link to="/"><h3 className="logo">PetPark</h3></Link>
+                <Link to="/"><span className="logo">Petreon</span></Link>
 
                 <Link to="/browse"><span className="browse">Browse Pets</span></Link>
 
-                <Link to="/aboutus"><span className="about">About Us</span></Link>
-
-                <input type="text" className="search" />
+                <input type="text" placeholder="Search..." className="search" />
+                <button className="button1"><img src="./loupe.png" alt="Search" className="glass" /></button>
 
                 <span className="login">Log In</span>
-                
+
                 <Link to="/signup"><span className="signup">Sign Up</span></Link>
+
             </div>
         )
     }
