c846d18cd785577d64a34984b1ad174046bd7a1a
Paul Stevens <paul.stevens.developer@gmail.com>
2021-01-16 21:42:19 -0800

    adds context

--- src/App.js
+++ src/App.js
@@ -15,79 +15,78 @@ import AboutUs from './AboutUs.js';
 import UserDash from './UserDash.js';
 import PetDetails from './PetDetails.js'
 import PostDetails from './PostDetails.js'
+import { MainContext } from './MainContext';
 
 export default class App extends Component {
   state = {
-    loginInfo: {},
-    userInfo: {}
-  }
-
-  handleAppState = async (object) => {
-    console.log(object)
-    await this.setState(object)
+    profile: "test",
+    loggedIn: false,
+    setProfile: (profile) => this.setState(profile),
+    logOut: () => this.setState({ loggedIn: false }),
+    logIn: () => this.setState({ loggedIn: true })
   }
 
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Header />
-          <Switch>
-            <Route
-              path="/"
-              exact render={(routerProps) => <LandingPage {...routerProps} />}
-            />
+      <MainContext.Provider value={this.state}>
+        <div className="App">
+          <Router>
+            <Header />
+            <Switch>
+              <Route
+                path="/"
+                exact render={(routerProps) => <LandingPage {...routerProps} />}
+              />
 
-            <Route
-              path="/login/:email/:firstName/:exisiting"
-              exact render={(routerProps) => <Login {...routerProps}
-                handleAppState={this.handleAppState}
-              />}
-            />
-            
-            <Route
-              path="/signup"
-              exact render={(routerProps) => <SignUp {...routerProps}
-                loginInfo={this.state.loginInfo}
-              />}
-            />
+              <Route
+                path="/login/:email/:firstName/:exisiting"
+                exact render={(routerProps) => <Login {...routerProps}
+                />}
+              />
 
-            <Route
-              path="/browse"
-              exact render={(routerProps) => <Browse {...routerProps} />}
-            />
+              <Route
+                path="/signup"
+                exact render={(routerProps) => <SignUp {...routerProps}
+                />}
+              />
 
-            <Route
-              path="/pets/:id"
-              exact render={(routerProps) => <PetDetails {...routerProps}
-                user={this.state.userInfo} />}
+              <Route
+                path="/browse"
+                exact render={(routerProps) => <Browse {...routerProps} />}
+              />
 
+              <Route
+                path="/pets/:id"
+                exact render={(routerProps) => <PetDetails {...routerProps}
+                  user={this.state.userInfo} />}
 
 
-            />
 
-            <Route
-              path="/posts/:id"
-              exact render={(routerProps) => <PostDetails {...routerProps}
-                user={this.state.userInfo} />}
-            />
+              />
 
-            <Route
-              path="/aboutus"
-              exact render={(routerProps) => <AboutUs {...routerProps} />}
-            />
+              <Route
+                path="/posts/:id"
+                exact render={(routerProps) => <PostDetails {...routerProps}
+                  user={this.state.userInfo} />}
+              />
 
-            <Route
-              path="/userdash"
-              exact render={(routerProps) => <UserDash {...routerProps}
-                user={this.state.userInfo} />}
+              <Route
+                path="/aboutus"
+                exact render={(routerProps) => <AboutUs {...routerProps} />}
+              />
+
+              <Route
+                path="/userdash"
+                exact render={(routerProps) => <UserDash {...routerProps}
+                  user={this.state.userInfo} />}
 
 
-            />
-          </Switch>
-          <Footer />
-        </Router>
-      </div>
+              />
+            </Switch>
+            <Footer />
+          </Router>
+        </div>
+      </MainContext.Provider>
     )
   }
 }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { createUser } from "./fetches/user-fetches.js"
+import { MainContext } from './MainContext.js'
 import './style/SignUp.css'
 
 
 export default class SignUp extends Component {
+    static contextType = MainContext;
+
     state = {
         userName: '',
         firstName: '',
@@ -13,8 +16,8 @@ export default class SignUp extends Component {
 
     componentDidMount = () => {
         this.setState({
-            email: this.props.loginInfo.email,
-            firstName: this.props.loginInfo.firstName,
+            email: this.context.profile.email,
+            firstName: this.context.profile.firstName,
         })
     }
 
@@ -23,9 +26,9 @@ export default class SignUp extends Component {
 
         this.setState({ loading: true })
         const user = new FormData(e.target)
-        console.log(...user)
-        console.log(e.target)
-        await createUser(user);
+        const newProfile = await createUser(user);
+        this.context.setProfile({ profile: newProfile })
+        this.context.logIn();
     }
 
     render() {
@@ -33,30 +36,30 @@ export default class SignUp extends Component {
             <div className='sup'>
                 <div className='naplesyellowborder'> </div>
 
-                    <h3 className='suhead'>Sign Up</h3>
+                <h3 className='suhead'>Sign Up</h3>
 
-                    <div className="box">
+                <div className="box">
 
                     <form onSubmit={this.handleSubmit}>
-                        
+
                         <p>Username:</p>
-                        <input 
+                        <input
                             className='userinput'
-                            name="userName" 
+                            name="userName"
                             maxLength="35"
                             onChange={(e) => this.setState({ userName: e.target.value })}
-                            value={this.state.userName} 
-                            />
-                        
+                            value={this.state.userName}
+                        />
+
                         <p>First Name:</p>
-                        <input 
+                        <input
                             className='nameinput'
                             name="firstName"
                             maxLength="35"
-                            onChange={(e) => this.setState({ firstName: e.target.value })}    
-                            value={this.state.firstName} 
-                            />
-                        
+                            onChange={(e) => this.setState({ firstName: e.target.value })}
+                            value={this.state.firstName}
+                        />
+
                         {/* <p>Profile Picture:</p>
                         <input 
                         type="file" 
@@ -65,23 +68,23 @@ export default class SignUp extends Component {
                         onChange={(e) => this.setState({ profilePicture: e.target.value })}
                             value={this.state.profilePicture}/>
                          */}
-                        
+
                         <p>Tagline:</p>
-                        <input 
-                            className='descinput' 
+                        <input
+                            className='descinput'
                             name="profileDescription"
                             maxLength="144"
                             onChange={(e) => this.setState({ profileDescription: e.target.value })}
-                            value={this.state.profileDescription} 
-                            />
-                        
+                            value={this.state.profileDescription}
+                        />
+
                         <br />
-                        
+
                         <button className='signup-button'>Submit</button>
-                        
+
                         <br />
                     </form>
-                    </div>
+                </div>
             </div>
         )
     }
