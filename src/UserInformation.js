75e1a91e3adf835df00c9c421974d9db15053276
Sjaan H <sjaanahydrick@gmail.com>
2021-01-17 18:14:13 -0800

    Feature/user dash creation (#33)
    * redirect on signup added
    
    * preparing for pull
    
    * profile picture upload functioning
    
    * userInformation styled 80%
    
    * toggling function added for profile picture upload
    
    * UserInformation fully styled

--- src/UserInformation.js
+++ src/UserInformation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ProfilePictureUpload from './ProfilePictureUpload.js';
 import { MainContext } from './MainContext.js'
+import './style/UserInfo.css'
 
 export default class UserInformation extends Component {
     static contextType = MainContext;
@@ -13,9 +14,17 @@ export default class UserInformation extends Component {
         return (
             <div className='uinfo'>
                 <h4 className="greeting">Hello, {this.context.profile.firstName}!</h4>
-                <img className="profpic" src={this.context.profile.profilePicture} alt='' width='150' height='150' />
-                <ProfilePictureUpload />
+
                 <h4 className="un">{this.context.profile.userName}</h4>
+
+                <img className="profpic" 
+                src={this.context.profile.profilePicture} 
+                alt='' 
+                width='150' 
+                height='150' />
+                
+                <ProfilePictureUpload />
+
                 <h5 className="prde">{this.context.profile.profileDescription}</h5>
             </div>
         )
