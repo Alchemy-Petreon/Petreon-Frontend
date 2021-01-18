20ffeb46b59422ee9d8f6bcd8843eb1f8b29a89b
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-18 10:32:32 -0800

    pet profile picture upload in progress

--- src/UserDashboard/PetProfilePictureUpload.js
+++ src/UserDashboard/PetProfilePictureUpload.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { uploadPetProfilePicture } from '../fetches/pet-fetches.js';
+import { MainContext } from '../MainContext';
 
 export default class PetProfilePictureUpload extends Component {
-    state = {
-        petProfilePicture: 'https://placekitten.com/250/250'
+    static contextType = MainContext;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            petProfilePicture: 'https://placekitten.com/250/250',
+            open: false
+        }
+        this.toggleUpload = this.toggleUpload.bind(this)
     }
 
+    toggleUpload(e) {
+        this.setState({ open: !this.state.open })
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const petpicture = new FormData(e.target)
+
+        const existingPet = await uploadPetProfilePicture(petpicture);
+    }
     render() {
         return (
             <div>
