7ac0a8b47a5330fb6a7600aea27ef9629ba8b2fa
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-17 18:57:50 -0800

    added fetchUserPets to pet-fetches and petArray to Pets.js

--- src/UserDashboard/Pets.js
+++ src/UserDashboard/Pets.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { fetchUserPets } from '../fetches/pet-fetches.js';
+import { MainContext } from '../MainContext.js';
+import '../style/Pets.css'
 
 export default class Pets extends Component {
+    static contextType = MainContext;
+    state = {
+        petArray: []
+    }
+
+    componentDidMount = async() => {
+        const petArray = await fetchUserPets(this.context.profile.id)
+
+        this.setState({
+            pets: petArray
+        });
+    };
+
     render() {
         return (
-            <div>
-                <h5 className='petsheader'>Pets:</h5>
+            <div className='petsdisplay'>
+                <h5 className='petsheader'>Pets</h5>
+
+                {this.state.petArray.length > 0 ? 
+                this.state.petArray.map(pet =>
+                    <div className='petthumbnail'>
+                    <p className='petthumbnailname'>{pet.petName}</p>
+                    <img className='petthumbnail' src={pet.petProfilePicture} alt='' />
+                    </div>)
+                : null } 
             </div>
         )
     }
