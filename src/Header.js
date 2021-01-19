6c070bb49f6f8bb9df2d68081a8de419ac87238c
Paul Stevens <paul.stevens.developer@gmail.com>
2021-01-18 21:51:12 -0800

    adds logout

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './style/Header.css';
 import { MainContext } from './MainContext.js'
 import { seedData } from './fetches/dev-fetches'
+import { logoutUser } from './fetches/user-fetches';
 
 
 export default class Header extends Component {
@@ -16,6 +17,12 @@ export default class Header extends Component {
         await seedData();
     }
 
+    handleLogout = async () => {
+        await logoutUser();
+        this.context.logOut();
+        this.context.setProfile('');
+    }
+
     render() {
         return (
             <div className="header">
@@ -25,11 +32,29 @@ export default class Header extends Component {
 
                 <input type="text" placeholder="Search..." className="search" />
                 <button className="button1"><img src="/loupe.png" alt="Search" className="glass" /></button>
+                {this.context.loggedIn ?
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <img
+                            src={this.context.profile.profilePicture}
+                            alt={this.context.profile.userName}
+                            style={{
+                                height: '60px',
+                                borderRadius: '50px'
+                            }}
+                        />
+                        <h3>{this.context.profile.userName}</h3>
+                        <button
+                            onClick={this.handleLogout}>
+                            Logout
+                        </button>
+                    </div>
+                    :
+                    <a href="https://petreon-api.herokuapp.com/api/v1/auth/google" className="login">Log In / Sign Up</a>
+                }
 
-                <a href="https://petreon-api.herokuapp.com/api/v1/auth/google" className="login">Log In</a>
 
                 {/* <Link to="/signup"><span className="signup">Sign Up</span></Link> */}
-                <button onClick={this.handleSeed}>SEED DATA</button>
+
 
             </div>
         )
--- src/fetches/user-fetches.js
+++ src/fetches/user-fetches.js
@@ -41,8 +41,8 @@ export async function createUser(newUser) {
             .post(`${URL}api/v1/users`)
             .send(newUser)
             .withCredentials()
-            return response.body;
-        } catch (err) {
+        return response.body;
+    } catch (err) {
         throw err;
     }
 }
@@ -88,6 +88,16 @@ export async function uploadProfilePicture(file) {
             .withCredentials()
             .send(file)
         return response.body;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function logoutUser() {
+    try {
+        const response = await request
+            .get(`${URL}api/v1/auth/logout`)
+            .withCredentials()
     } catch (err) {
         throw err;
     }
