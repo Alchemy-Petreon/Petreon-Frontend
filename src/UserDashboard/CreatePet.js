63993f3cd33e01728489b1faf1bfa2e04b2b4924
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-21 10:40:50 -0800

    adjusted maxLength on inputs in CreatePet

--- src/UserDashboard/CreatePet.js
+++ src/UserDashboard/CreatePet.js
@@ -204,7 +204,7 @@ export default class CreatePet extends PureComponent {
                             <textarea
                                 rows="1"
                                 name='petProfileDescription'
-                                maxLength='750'
+                                maxLength='550'
                                 className='petdesc'
                                 placeholder={this.state.petProfileDescription}
                                 onChange={(e) => this.setState({ petProfileDescription: e.target.value })}
