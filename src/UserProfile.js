4bb23c9fb7161ba7e72a9e7234ab54fd36d7ef1e
Sjaan H <sjaanahydrick@gmail.com>
2021-01-21 17:06:19 -0800

    Fix/random styling (#95)
    * added loading spinner file
    
    * added push to dash on logout in Header.js
    
    * fixed spacing in Footer
    
    * snazzed up landing page blurbs; fixed margins on landing page
    
    * adjusted margins on ProfilePictureUpload
    
    * consolidated Subscriptions.css into UserDash.css; deleted Subscriptions.css
    
    * user dashboard subscription display designed
    
    * adjusted maxLength on inputs in CreatePet
    
    * fixed spacing in user dash for edgecase pet
    
    * adjusted maxLength for SignUp.js
    
    * adjusted spacing for edgecase pet in PetDash
    
    * adjusted spacing and styled Update Pet button on PetDash
    
    * deleted commented out reference to video in CreatePost
    
    * fixed spacing on caption in CreatePost
    
    * adjusted length for postText
    
    * fixed spacing on userdash
    
    * preparing for merge
    
    * styled qr code on PetDetails page
    
    * added className to loading spinners
    
    * fixed spacing for loading spinner
    
    * added like icons
    
    * deleted commented out lines UpdatePet
    
    * removed commented out lines UserProfile
    
    * added styling to user-profile-info in UserProfile

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-// import { MainContext } from './MainContext.js';
 import { fetchUser } from "./fetches/user-fetches.js"
 import { fetchUserPets } from './fetches/pet-fetches.js';
 import { Link } from 'react-router-dom';
 
 var QRCode = require('qrcode.react');
 
-// React.render(
-//     <QRCode value="http://facebook.github.io/react/" />,
-
-// );
-
-
 export default class UserProfile extends Component {
-    // static contextType = MainContext;
     state = {
         user: {},
         petArray: {}
-
     }
     
     componentDidMount = async () => {
@@ -36,22 +27,30 @@ export default class UserProfile extends Component {
     render() {
         return (
             <section>
-                <QRCode value={`venmo://paycharge?txn=pay&recipients=${this.state.user.venmo}&amount=1&note=For-${this.state.user.firstName}s-pets`} />,
+                <div className='user-profile'>
+                    <div className='upbittersweet'> </div>
+                    <div className='upnaplesyellow'> </div>
+                    <div className='user-profile-info'>
+                        <p className='user-profile-userName'>{this.state.user.userName}</p>
+                        <img src={this.state.user.profilePicture} className='user-profile-profilePicture' alt='profile' />
+                        <p className='user-profile-profileDescription'>{this.state.user.profileDescription}</p>
+                        <QRCode value={`venmo://paycharge?txn=pay&recipients=${this.state.user.venmo}&amount=1&note=For-${this.state.user.firstName}s-pets`} />
+                    </div>
 
-                <div>
-                    <p>{this.state.user.firstName}</p>
-                    <p>{this.state.user.userName}</p>
-                    <img src={this.state.user.profilePicture} alt='profile' />
-                    <p>{this.state.user.profileDescription}</p>
-                </div>
-
-                <div>
-                    <p>Pets</p>
-                    {this.state.petArray.length > 0 ?
-                        this.state.petArray.map(pet =>
-                            <div> <p>{pet.petName}</p>
-                                <Link to={`/pets/${pet.id}`}><img src={pet.petProfilePicture} alt='pet' /></Link></div>)
-                        : null}
+                    <div>
+                        <p className='user-profile-pets-header'>Pets</p>
+                        <div className='user-profile-pet-list'>
+                        {this.state.petArray.length > 0 ?
+                            this.state.petArray.map(pet =>
+                                <div className='user-profile-pets'> 
+                                    <Link to={`/pets/${pet.id}`}>
+                                    <p className='user-profile-petName'>{pet.petName}</p>
+                                    <img src={pet.petProfilePicture} className='user-profile-pet-picture' alt='pet' />
+                                    </Link>
+                                </div>)
+                            : null}
+                        </div> 
+                    </div>
                 </div>
             </section>
         )
