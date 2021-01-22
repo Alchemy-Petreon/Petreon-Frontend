cf021ce8835cf731db594cc26c53905d3a863df4
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-01-22 09:30:14 -0800

    fixed spacing in Browse.js

--- src/Browse.js
+++ src/Browse.js
@@ -10,6 +10,7 @@ export default class Browse extends Component {
         loading: true,
         allPets: [],
     }
+
     componentDidMount = async () => {
         await this.setState({ loading: true });
         const allPets = await fetchPets();
@@ -21,13 +22,13 @@ export default class Browse extends Component {
 
     handleSearch = async (e) => {
         e.preventDefault();
+
         await this.setState({
             loading: true
         })
 
-        console.log(this.state.searchField)
         const newPetArray = await searchResults(this.state.searchField)
-        console.log(newPetArray)
+
         await this.setState({
             loading: false,
             allPets: newPetArray
@@ -39,9 +40,12 @@ export default class Browse extends Component {
             <div>
                 <div className='browsebittersweet'> </div>
                 <div className='browsenaplesyellow'> </div>
+
                 <div className='search-bar'>
                     <form onSubmit={this.handleSearch}>
-                        <input type="text" placeholder="Search..." className="search"
+                        <input 
+                            type="text" 
+                            placeholder="Search..." className="search"
                             name="search"
                             onChange={(e) => this.setState({ searchField: e.target.value })}
                         />
@@ -54,6 +58,7 @@ export default class Browse extends Component {
                         </button>
                     </form>
                 </div>
+
                 <div className='pet-box'>
                     {this.state.loading
                         ? <img src={'/loading-spinner.gif'} className='loading-spinner' alt={''} />
@@ -61,12 +66,10 @@ export default class Browse extends Component {
                         this.state.allPets.map(pet =>
                             <Link to={`/pets/${pet.id}`} >
                                 <div className='pet-box' key={pet.id}>
-                                    <PetItem
-                                        pet={pet} />
+                                    <PetItem pet={pet} />
                                 </div>
                             </Link>)
                     }
-
                 </div>
             </div>
         )
