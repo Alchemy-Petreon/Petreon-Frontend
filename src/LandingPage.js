cfe8e81a943a587d1739702794dddff9cc2ade37
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-01-21 18:50:20 -0800

    moves images to s3

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import './style/LandingPage.css';
 
+const s3 = 'https://petreon.s3-us-west-2.amazonaws.com/src'
+
 export default class LandingPage extends Component {
     render() {
         return (
             <div className="lp">
 
                 <div className="banner">
-                    <img src="/minimalcolorizing.jpg" alt="kitties" className="landing-banner"/>
+                    <img src="/minimalcolorizing.jpg" alt="kitties" className="landing-banner" />
                 </div>
 
                 <div className="colors">
                     <div className="midnightgreen">
-                    <h1 className="flaunt">Flaunt Your Furbaby</h1>
+                        <h1 className="flaunt">Flaunt Your Furbaby</h1>
                     </div>
                     <div className="bittersweet"> </div>
                     <div className="naplesyellow"> </div>
@@ -21,7 +23,7 @@ export default class LandingPage extends Component {
                 <div className="intro">
                     <h2 className="showcase">Promote your pet!</h2>
                     <p className="lp1">Unleash your best friend on the world by sharing images or blogs! Build a community with fans by liking posts and leaving comments!</p>
-                    <img src="/ben1.jpg" alt="ben" className="ben" />
+                    <img src={`${s3}/ben1.jpg`} alt="ben" className="ben" />
 
                     <div className="divider1">
                         <div className="midnightgreendiv1"> </div>
@@ -31,7 +33,7 @@ export default class LandingPage extends Component {
 
                     <h2 className="follow">Follow your favorites!</h2>
                     <p className="lp2">Only want to see cats, dogs, ferrets, or frogs? You have the freedom to curate your dashboard and only see the content from the animals you love the most!</p>
-                    <img src="/Vinewood.png" alt="vinewood" className="vinewood" />
+                    <img src={`${s3}/Vinewood.png`} alt="vinewood" className="vinewood" />
 
                     <div className="divider2">
                         <div className="midnightgreendiv2"> </div>
@@ -41,7 +43,7 @@ export default class LandingPage extends Component {
 
                     <h2 className="monetize">Monetize your monster!</h2>
                     <p className="lp3">Does your pup need a new collar? Could your cat benefit from a new toy? Accept tips from people who want the best for your beast or buy a treat for your lovable new friend!</p>
-                    <img src="/Wallace2.jpg" alt="wallace" className="wallace" />
+                    <img src={`${s3}/Wallace2.jpg`} alt="wallace" className="wallace" />
 
                     <div className="divider3">
                         <div className="midnightgreendiv3"> </div>
